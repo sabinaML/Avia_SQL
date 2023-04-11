6b6fee48c4e91ed198dd1d7432a199d04302a03a
Albina S <90548660+sabinaML@users.noreply.github.com>
2023-04-11 19:32:25 +0300

    Update train.sql

--- train.sql
+++ train.sql
@@ -3,3 +3,4 @@ surface_name,  sum(revenue) revenue
 from transactions
 group by date_trunc('month', invoice_created_dt) , service_name,
 surface_name
+![Выручка](https://github.com/sabinaML/Pictures/blob/main/Screenshot_2.png)
