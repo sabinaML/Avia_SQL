5f86b86cd39db7f66d99b1a61de8827f2be44449
Albina S <90548660+sabinaML@users.noreply.github.com>
2023-04-11 09:46:44 +0300

    Add files via upload

---
+++ train.sql
@@ -0,0 +1,5 @@
+select date_trunc('month', invoice_created_dt) day_ , service_name, 
+surface_name,  sum(revenue) revenue 
+from transactions
+group by date_trunc('month', invoice_created_dt) , service_name,
+surface_name
